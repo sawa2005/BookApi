a41e07f71fea2d35aa86dd4b7ca7f19f917fcb0c
Samuel Ward <samuelleeward9@gmail.com>
2025-06-16 23:18:37 +0200

    automatic db migration added

--- server/Program.cs
+++ server/Program.cs
@@ -85,6 +85,12 @@ builder.Services.AddAuthorization();
 
 var app = builder.Build();
 
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+    db.Database.Migrate();
+}
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
